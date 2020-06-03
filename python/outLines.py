b4e33e7c069b2a2d54e16bf1934ba42f9afa808e
seiya2214 <seiya2214@yahoo.co.jp>
2020-06-03 11:15:24 +0900

    outLines.pyの変更

--- python/outLines.py
+++ python/outLines.py
@@ -1,11 +1,12 @@
 import cv2
 from glob import glob
 
-IMAGE_PATH = "./images"
-OUT_IMAGE_PATH = "./outLineImage"
+IMAGE_PATH = "./images/human"
 
 for file in glob(IMAGE_PATH + '/*.jpg'):
     img = cv2.imread(file)
     img_mask = cv2.Canny(img, 30, 200, apertureSize = 3) # マスクを作成
     result = cv2.bitwise_and(img, img, mask=img_mask) # 元画像とマスクを合成
-    cv2.imwrite(file, result)
+    rename_file = file.replace("images", "outLineImages")
+    print(rename_file)
+    cv2.imwrite(rename_file, result)
